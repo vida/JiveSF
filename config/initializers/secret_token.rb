a10de5e94ddab74eb19634af901ea7ce41de066d
Vida Ha <vida@ifeelgoods.com>
2011-12-01 19:43:24 -0800

    Uploading all new stuff from the old Jive Website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JiveSF::Application.config.secret_token = 'a6efc6ac6e3470d548b59a8165d66a9b599a35887aebaf1b5ef04645953100294c96dd2da838b0710823a40d618ed143867d80c04d15df824c1d3220b1c79f15'
